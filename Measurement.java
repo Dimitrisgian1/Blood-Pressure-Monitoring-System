834bc83615c2da9b550aec652637cf651573152a
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2020-03-06 18:56:06 +0200

    Update Measurement.java

--- Measurement.java
+++ Measurement.java
@@ -1,12 +1,19 @@
 package BPMS;
 
+import java.util.Random;
+
 public class Measurement {
 
-    int date;
-    int time;
-    float systolicBP;
-    float diastolicBP;
-    float heartRate;
-    int userid;
+    static Random rand = new Random();
 
+    static int date = rand.nextInt(1000);
+    static int time = rand.nextInt(100);
+    static float systolicBP = rand.nextFloat() * 10;
+    static float diastolicBP = rand.nextFloat() * 10;
+    static float heartRate = rand.nextFloat() * 180;
+    static int userid = rand.nextInt(3);
+
+    public static void PRINT() {
+        System.out.println(date + "2020 " + time + " " + systolicBP + " " + diastolicBP + " " + heartRate + " " + userid);
+    }
 }
