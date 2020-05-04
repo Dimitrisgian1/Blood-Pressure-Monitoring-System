38b74397852ff9b57d4053644762324e7c9c08f9
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2020-05-04 15:25:05 +0300

    major overhaul

--- Measurement.java
+++ Measurement.java
@@ -1,19 +1,43 @@
 package BPMS;
 
-import java.util.Random;
+import java.io.Serializable;
+import java.sql.Date;
 
-public class Measurement {
+public class Measurement implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
 
-    static Random rand = new Random();
+    private int userID;
+    public java.util.Date date;
+    public int systolicBP;
+    public int diastolicBP;
+    public int heartRate;
 
-    static int date = rand.nextInt(1000);
-    static int time = rand.nextInt(100);
-    static float systolicBP = rand.nextFloat() * 10;
-    static float diastolicBP = rand.nextFloat() * 10;
-    static float heartRate = rand.nextFloat() * 180;
-    static int userid = rand.nextInt(3);
+    
+    public Measurement(int uid, java.util.Date d, int s, int dbp, int hr) 
+    {
+        this.userID = uid;
+        this.date = d;
+        this.systolicBP = s;
+        this.diastolicBP = dbp;
+        this.heartRate = hr;
+    }
 
-    public static void PRINT() {
-        System.out.println(date + "2020 " + time + " " + systolicBP + " " + diastolicBP + " " + heartRate + " " + userid);
+    public Measurement() {}
+
+    public void print() 
+    {
+        System.out.println("UserID:" + this.userID + "\tDate:" + this.date.toString() + "\tsystolicBP:" + this.systolicBP + "\tdiastolicBP:" + this.diastolicBP + "\thartRate:" + this.heartRate);
+    }
+
+        
+    public String toString() 
+    {
+        return this.userID + "\t\t|" + this.date.toString() + "\t|" + Integer.toString(this.systolicBP) + "\t\t|" + Integer.toString(this.diastolicBP) + "\t\t|" + Integer.toString(this.heartRate) + "\t|\n";
+    }
+
+    public int getUserId() 
+    {
+        return this.userID;
     }
 }
