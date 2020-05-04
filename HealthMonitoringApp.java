1379a8106558237705a2d590bae53df9dede987d
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2020-05-04 15:24:26 +0300

    added GUI

--- HealthMonitoringApp.java
+++ HealthMonitoringApp.java
@@ -1,11 +1,25 @@
 package BPMS;
 
-public class HealthMonitoringApp {
+public class HealthMonitoringApp 
+{
 
-	public static void main(String[] args) {
-		
-        // BPMS.fetch();
-        Measurement.PRINT();
-	}
+    static BPBook bpBook;
+    static BPMSGui gui;
+
+    public static void main(String[] args) 
+    {
+        bpBook = new BPBook();
+        gui = new BPMSGui(bpBook);
+
+        TCPServerBPMS s = new TCPServerBPMS(8080 /*Integer.parseInt(args[2])*/ );
+        s.start();
+    }
+  
+    public static void setBPBook(BPBook bpBook2) 
+    {
+        bpBook = bpBook2;
+        gui.setBpBook(bpBook);
+    }
+	
 
 }
